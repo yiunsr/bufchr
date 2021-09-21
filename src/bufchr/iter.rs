e44a08f9bb9b8e549390904b933570157c4d4207
yiunsr <yiunsr@hanmail.net>
2021-09-21 21:55:19 +0900

    performance tuning using start_align_pos - first time faster than memchr

--- src/bufchr/iter.rs
+++ src/bufchr/iter.rs
@@ -3,14 +3,14 @@ use crate::bufchr::CbBufchr;
 use crate::bufchr::CbBufchr2;
 use crate::bufchr::CbBufchr3;
 
-
 /// struct used when there is only one needle
 pub struct Bufchr<'a> {
     haystack: &'a [u8],
     needle0: u8,
     position: usize,
     cache: u64,
-    vector_size: usize,
+    batch_byte_size: usize,
+    start_align_pos: usize,
     vector_end_ptr: *const u8,
     cb_bufchr: CbBufchr,
 }
@@ -18,75 +18,78 @@ impl<'a> Bufchr<'a> {
     /// Needle is what you are trying to find and the location you are looking for is haystack.
     #[inline]
     pub fn new(haystack: &[u8], needle0: u8) -> Bufchr<'_> {
-        let vector_size = bufchr::get_batch_byte_size();
+        let batch_byte_size = bufchr::get_batch_byte_size();
         let cb_bufchr = bufchr::get_cb_bufchr();
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
+        
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
 
         Bufchr {haystack: haystack, needle0: needle0,
-            position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr: cb_bufchr}
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr: cb_bufchr,
+            start_align_pos: 0, vector_end_ptr: vector_end_ptr,
+        }
     }
 
     #[doc(hidden)]
     #[inline]
     pub fn new_avx(haystack: &[u8], needle0: u8) -> Bufchr<'_> {
-        let vector_size = bufchr::avx::get_batch_byte_size();
+        let batch_byte_size = bufchr::avx::get_batch_byte_size();
         let cb_bufchr = bufchr::avx::bufchr;
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr {haystack: haystack, needle0: needle0,
-            position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr: cb_bufchr}
+            Bufchr {haystack: haystack, needle0: needle0,
+                position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr: cb_bufchr,
+                start_align_pos: 0 ,vector_end_ptr: vector_end_ptr,
+            }
     }
 
     #[doc(hidden)]
     #[inline]
     pub fn new_sse2(haystack: &[u8], needle0: u8) -> Bufchr<'_> {
-        let vector_size = bufchr::sse2::get_batch_byte_size();
+        let batch_byte_size = bufchr::sse2::get_batch_byte_size();
         let cb_bufchr = bufchr::sse2::bufchr;
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
         Bufchr {haystack: haystack, needle0: needle0,
-            position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr: cb_bufchr}
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr: cb_bufchr,
+            start_align_pos: 0 , vector_end_ptr: vector_end_ptr,
+        }
     }
 }
 impl<'a> Iterator for Bufchr<'a> {
     type Item = usize;
-    
 
     /// The needle position is returned. If there is no needle, None is returned.
     #[inline]
     fn next(&mut self) -> Option<usize> {
         if self.cache != 0 {
-            let start_align_pos = (self.position / self.vector_size) * self.vector_size;
             let bit_pos = self.cache.trailing_zeros() as usize;
-            self.cache = self.cache & !(1 << bit_pos);
-            self.position = start_align_pos + bit_pos + 1;
+            // Reset lowest set bit	
+            self.cache = self.cache & (self.cache - 1);
+            self.position = self.start_align_pos + bit_pos + 1;
             return Some(self.position);
         }
         let align_pos;
         if self.position == 0 {
             align_pos = 0;
         }
-        else if self.haystack.len() - self.position < self.vector_size{
+        else if self.haystack.len() - self.position < self.batch_byte_size{
             align_pos = self.position;
         }
         else{
-            align_pos = ( (self.position - 1) / self.vector_size + 1) * self.vector_size;
+            align_pos = ( (self.position - 1) / self.batch_byte_size + 1) * self.batch_byte_size;
         }
         let haystack_len = self.haystack.len() - align_pos;
         let new_haystack;
@@ -103,6 +106,9 @@ impl<'a> Iterator for Bufchr<'a> {
         match position {
             Some(pos) => {
                 self.position = align_pos + pos + 1;
+                if self.cache != 0 {
+                    self.start_align_pos = get_vector_start_ptr(self.position, self.batch_byte_size);
+                }
             }
             None =>{
                 return None;
@@ -120,7 +126,8 @@ pub struct Bufchr2<'a> {
     needle1: u8,
     position: usize,
     cache: u64,
-    vector_size: usize,
+    batch_byte_size: usize,
+    start_align_pos: usize,
     vector_end_ptr: *const u8,
     cb_bufchr2: CbBufchr2,
 }
@@ -128,48 +135,51 @@ impl<'a> Bufchr2<'a> {
     /// needle0, needle1 are what you are trying to find and the location you are looking for is haystack.
     #[inline]
     pub fn new(haystack: &[u8], needle0: u8, needle1: u8) -> Bufchr2<'_> {
-        let vector_size = bufchr::get_batch_byte_size();
+        let batch_byte_size = bufchr::get_batch_byte_size();
         let cb_bufchr2 = bufchr::get_cb_bufchr2();
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr2 { haystack: haystack, needle0: needle0, needle1: needle1,
-            position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr2: cb_bufchr2 }
+        Bufchr2 {haystack: haystack, needle0: needle0, needle1: needle1,
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr2: cb_bufchr2,
+            start_align_pos: 0 , vector_end_ptr: vector_end_ptr,
+        }
     }
 
     #[doc(hidden)]
     #[inline]
     pub fn new_avx(haystack: &[u8], needle0: u8, needle1: u8) -> Bufchr2<'_> {
-        let vector_size = bufchr::avx::get_batch_byte_size();
+        let batch_byte_size = bufchr::avx::get_batch_byte_size();
         let cb_bufchr2 = bufchr::avx::bufchr2;
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr2 { haystack: haystack, needle0: needle0, needle1: needle1,
-            position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr2: cb_bufchr2 }
+        Bufchr2 {haystack: haystack, needle0: needle0, needle1: needle1,
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr2: cb_bufchr2,
+            start_align_pos: 0, vector_end_ptr: vector_end_ptr,
+        }
     }
 
     #[inline]
     pub fn new_sse2(haystack: &[u8], needle0: u8, needle1: u8) -> Bufchr2<'_> {
-        let vector_size = bufchr::sse2::get_batch_byte_size();
+        let batch_byte_size = bufchr::sse2::get_batch_byte_size();
         let cb_bufchr2 = bufchr::sse2::bufchr2;
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr2 { haystack: haystack, needle0: needle0, needle1: needle1,
-            position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr2: cb_bufchr2 }
+        Bufchr2 {haystack: haystack, needle0: needle0, needle1: needle1,
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr2: cb_bufchr2,
+            start_align_pos: 0 , vector_end_ptr: vector_end_ptr,
+        }
     }
 }
 impl<'a> Iterator for Bufchr2<'a> {
@@ -179,21 +189,21 @@ impl<'a> Iterator for Bufchr2<'a> {
     #[inline]
     fn next(&mut self) -> Option<usize> {
         if self.cache != 0 {
-            let start_align_pos = (self.position / self.vector_size) * self.vector_size;
             let bit_pos = self.cache.trailing_zeros() as usize;
-            self.cache = self.cache & !(1 << bit_pos);
-            self.position = start_align_pos + bit_pos + 1;
+            // Reset lowest set bit	
+            self.cache = self.cache & (self.cache - 1);
+            self.position = self.start_align_pos + bit_pos + 1;
             return Some(self.position);
         }
         let align_pos;
         if self.position == 0 {
             align_pos = 0;
         }
-        else if self.haystack.len() - self.position < self.vector_size{
+        else if self.haystack.len() - self.position < self.batch_byte_size{
             align_pos = self.position;
         }
         else{
-            align_pos = ( (self.position - 1) / self.vector_size + 1) * self.vector_size;
+            align_pos = ( (self.position - 1) / self.batch_byte_size + 1) * self.batch_byte_size;
         }
         let haystack_len = self.haystack.len() - align_pos;
         let new_haystack;
@@ -213,6 +223,9 @@ impl<'a> Iterator for Bufchr2<'a> {
         match position {
             Some(pos) => {
                 self.position = align_pos + pos + 1;
+                if self.cache != 0 {
+                    self.start_align_pos = get_vector_start_ptr(self.position, self.batch_byte_size);
+                }
             }
             None =>{
                 return None;
@@ -231,7 +244,8 @@ pub struct Bufchr3<'a> {
     needle2: u8,
     position: usize,
     cache: u64,
-    vector_size: usize,
+    batch_byte_size: usize,
+    start_align_pos: usize,
     vector_end_ptr: *const u8,
     cb_bufchr3: CbBufchr3,
 }
@@ -239,49 +253,52 @@ impl<'a> Bufchr3<'a> {
     /// needle0, needle1, needle2 are what you are trying to find and the location you are looking for is haystack.
     #[inline]
     pub fn new(haystack: &[u8], needle0: u8, needle1: u8, needle2: u8) -> Bufchr3<'_> {
-        let vector_size = bufchr::get_batch_byte_size();
+        let batch_byte_size = bufchr::get_batch_byte_size();
         let cb_bufchr3 = bufchr::get_cb_bufchr3();
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr3 { haystack: haystack, needle0: needle0, needle1: needle1,
-            needle2: needle2, position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr3: cb_bufchr3 }
+        Bufchr3 {haystack: haystack, needle0: needle0, needle1: needle1, needle2: needle2,
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr3: cb_bufchr3,
+            start_align_pos: 0, vector_end_ptr: vector_end_ptr,
+        }
     }
 
     #[doc(hidden)]
     #[inline]
     pub fn new_avx(haystack: &[u8], needle0: u8, needle1: u8, needle2: u8) -> Bufchr3<'_> {
-        let vector_size = bufchr::avx::get_batch_byte_size();
+        let batch_byte_size = bufchr::avx::get_batch_byte_size();
         let cb_bufchr3 = bufchr::avx::bufchr3;
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr3 { haystack: haystack, needle0: needle0, needle1: needle1,
-            needle2: needle2, position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr3: cb_bufchr3 }
+        Bufchr3 {haystack: haystack, needle0: needle0, needle1: needle1, needle2: needle2,
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr3: cb_bufchr3,
+            start_align_pos: 0, vector_end_ptr: vector_end_ptr,
+        }
     }
 
     #[doc(hidden)]
     #[inline]
     pub fn new_sse2(haystack: &[u8], needle0: u8, needle1: u8, needle2: u8) -> Bufchr3<'_> {
-        let vector_size = bufchr::sse2::get_batch_byte_size();
+        let batch_byte_size = bufchr::sse2::get_batch_byte_size();
         let cb_bufchr3 = bufchr::sse2::bufchr3;
         let haystack_len = haystack.len();
         let start_ptr = haystack.as_ptr();
         let vector_end_ptr = 
             unsafe{
-                start_ptr.add((haystack_len / vector_size) * vector_size)
+                start_ptr.add((haystack_len / batch_byte_size) * batch_byte_size)
             };
-        Bufchr3 { haystack: haystack, needle0: needle0, needle1: needle1,
-            needle2: needle2, position: 0, cache: 0,
-            vector_size: vector_size, vector_end_ptr: vector_end_ptr, cb_bufchr3: cb_bufchr3 }
+        Bufchr3 {haystack: haystack, needle0: needle0, needle1: needle1, needle2: needle2,
+            position: 0, cache: 0, batch_byte_size: batch_byte_size, cb_bufchr3: cb_bufchr3,
+            start_align_pos: 0, vector_end_ptr: vector_end_ptr,
+        }
     }
 }
 impl<'a> Iterator for Bufchr3<'a> {
@@ -291,21 +308,21 @@ impl<'a> Iterator for Bufchr3<'a> {
     #[inline]
     fn next(&mut self) -> Option<usize> {
         if self.cache != 0 {
-            let start_align_pos = (self.position / self.vector_size) * self.vector_size;
             let bit_pos = self.cache.trailing_zeros() as usize;
-            self.cache = self.cache & !(1 << bit_pos);
-            self.position = start_align_pos + bit_pos + 1;
+            // Reset lowest set bit	
+            self.cache = self.cache & (self.cache - 1);
+            self.position = self.start_align_pos + bit_pos + 1;
             return Some(self.position);
         }
         let align_pos;
         if self.position == 0 {
             align_pos = 0;
         }
-        else if self.haystack.len() - self.position < self.vector_size{
+        else if self.haystack.len() - self.position < self.batch_byte_size{
             align_pos = self.position;
         }
         else{
-            align_pos = ( (self.position - 1) / self.vector_size + 1) * self.vector_size;
+            align_pos = ( (self.position - 1) / self.batch_byte_size + 1) * self.batch_byte_size;
         }
         let haystack_len = self.haystack.len() - align_pos;
         let new_haystack;
@@ -325,6 +342,9 @@ impl<'a> Iterator for Bufchr3<'a> {
         match position {
             Some(pos) => {
                 self.position = align_pos + pos + 1;
+                if self.cache != 0 {
+                    self.start_align_pos = get_vector_start_ptr(self.position, self.batch_byte_size);
+                }
             }
             None =>{
                 return None;
@@ -338,4 +358,9 @@ impl<'a> Iterator for Bufchr3<'a> {
 #[inline]
 fn forward_pos(mask: u32) -> usize {
     mask.trailing_zeros() as usize
+}
+
+#[inline(always)]
+fn get_vector_start_ptr(position: usize, batch_byte_size: usize) -> usize {
+    (position / batch_byte_size) * batch_byte_size
 }
